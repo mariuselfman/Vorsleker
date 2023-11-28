6bfc84110dfa1f31829eb02cfd8be7e47edec144
mariuselfman <marius.elfman@gmail.com>
2023-11-28 14:14:53 +0100

    Initial Commit

---
+++ Vorsleker/VorslekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  VorslekerApp.swift
+//  Vorsleker
+//
+//  Created by Marius Bækken on 28/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct VorslekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
